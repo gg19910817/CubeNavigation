5c73d8d091433268b648fa2c9cfc96b5c09b88cc
gg19910817 <gg19910817@gmail.com>
2020-06-17 16:16:36 +0800

    init

---
+++ Example/Pods/Target Support Files/CubeNavigation/CubeNavigation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CubeNavigation : NSObject
+@end
+@implementation PodsDummy_CubeNavigation
+@end
---
+++ Example/Pods/Target Support Files/Pods-CubeNavigation_Tests/Pods-CubeNavigation_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CubeNavigation_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CubeNavigation_Tests
+@end
